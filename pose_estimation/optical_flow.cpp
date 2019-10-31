a3d65351dc2cfbf69fd8c2e19de73dd0e05a46e8
cggos <cggos@outlook.com>
2019-10-31 10:41:18 +0800

    use buildOpticalFlowPyramid

--- pose_estimation/optical_flow.cpp
+++ pose_estimation/optical_flow.cpp
@@ -18,11 +18,11 @@ using namespace cv;
  * @param [in] inverse use inverse formulation?
  */
 void OpticalFlowSingleLevel(
-        const Mat &img1,
-        const Mat &img2,
-        const vector<KeyPoint> &kp1,
-        vector<KeyPoint> &kp2,
-        vector<bool> &success,
+        const cv::Mat &img1,
+        const cv::Mat &img2,
+        const vector <cv::Point2f> &kpt1,
+        vector <cv::Point2f> &kpt2,
+        vector<unsigned char> &success,
         bool inverse = false
 );
 
@@ -37,11 +37,11 @@ void OpticalFlowSingleLevel(
  * @param [in] inverse set true to enable inverse formulation
  */
 void OpticalFlowMultiLevel(
-        const vector<Mat> &pyr1,
-        const vector<Mat> &pyr2,
-        const vector<KeyPoint> &kp1,
-        vector<KeyPoint> &kp2,
-        vector<bool> &success,
+        const vector <cv::Mat> &pyr1,
+        const vector <cv::Mat> &pyr2,
+        const vector <cv::Point2f> &kpt1,
+        vector <cv::Point2f> &kpt2,
+        vector<unsigned char> &success,
         bool inverse = true
 );
 
@@ -91,44 +91,59 @@ int main(int argc, char **argv) {
     Ptr<GFTTDetector> detector = GFTTDetector::create(500, 0.01, 20); // maximum 500 keypoints
     detector->detect(img1, kp1);
 
-    // now lets track these key points in the second image
-    // first use single level LK in the validation picture
-    vector<KeyPoint> kp2_single;
-    vector<bool> success_single;
-    OpticalFlowSingleLevel(img1, img2, kp1, kp2_single, success_single);
+    vector<Point2f> cv_pt1;
+    for (auto &kp: kp1)
+        cv_pt1.push_back(kp.pt);
 
-    // then test multi-level LK
+    // now lets track these key points in the second image
+
+    /// first use single level LK in the validation picture
+    vector<Point2f> kp2_single;
+    vector<unsigned char> success_single;
+    OpticalFlowSingleLevel(img1, img2, cv_pt1, kp2_single, success_single);
+
+    /// then test multi-level LK
     // create pyramids
     double scale = 1.0;
     vector<Mat> pyr1, pyr2; // image pyramids
     Mat tmp1, tmp2;
     for (int i = 0; i < 4; i++) {
-        cv::resize(img1, tmp1, Size(img1.cols*scale, img1.rows*scale));
+        cv::resize(img1, tmp1, Size(img1.cols * scale, img1.rows * scale));
         pyr1.push_back(tmp1);
-        cv::resize(img2, tmp2, Size(img2.cols*scale, img2.rows*scale));
+        cv::resize(img2, tmp2, Size(img2.cols * scale, img2.rows * scale));
         pyr2.push_back(tmp2);
         scale *= 0.5;
     }
 
-    vector<KeyPoint> kp2_multi;
-    vector<bool> success_multi;
-    OpticalFlowMultiLevel(pyr1, pyr2, kp1, kp2_multi, success_multi);
+    vector<Point2f> kp2_multi;
+    vector<unsigned char> success_multi;
+    OpticalFlowMultiLevel(pyr1, pyr2, cv_pt1, kp2_multi, success_multi);
 
-    // use opencv's flow for validation
-    vector<Point2f> pt1, pt2;
-    for (auto &kp: kp1)
-        pt1.push_back(kp.pt);
-    vector<uchar> status;
-    vector<float> error;
-    cv::calcOpticalFlowPyrLK(img1, img2, pt1, pt2, status, error, cv::Size(8, 8));
+    /// use opencv's flow for validation
+    vector<Point2f> cv_pt2_single;
+    vector<uchar> status_single;
+    vector<float> error_single;
+    cv::calcOpticalFlowPyrLK(img1, img2, cv_pt1, cv_pt2_single, status_single, error_single, cv::Size(8, 8));
+
+    /// use opencv's multi-level pyramids lk optical flow
+    std::vector<cv::Mat> img1_pyramid, img2_pyramid;
+    cv::buildOpticalFlowPyramid(img1, img1_pyramid, cv::Size(15, 15), 3, true, cv::BORDER_REFLECT_101, cv::BORDER_CONSTANT, false);
+    cv::buildOpticalFlowPyramid(img2, img2_pyramid, cv::Size(15, 15), 3, true, cv::BORDER_REFLECT_101, cv::BORDER_CONSTANT, false);
+
+    vector<Point2f> cv_pt2_multi;
+    vector<uchar> status_multi;
+    cv::calcOpticalFlowPyrLK(
+            img1_pyramid, img2_pyramid, cv_pt1, cv_pt2_multi, status_multi, cv::noArray(), cv::Size(15, 15), 3,
+            cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 0.01));
+
 
     // plot the differences of those functions
     Mat img2_single;
     cv::cvtColor(img2, img2_single, CV_GRAY2BGR);
     for (int i = 0; i < kp2_single.size(); i++) {
         if (success_single[i]) {
-            cv::circle(img2_single, kp2_single[i].pt, 2, cv::Scalar(0, 250, 0), 2);
-            cv::line(img2_single, kp1[i].pt, kp2_single[i].pt, cv::Scalar(0, 250, 0));
+            cv::circle(img2_single, kp2_single[i], 2, cv::Scalar(0, 250, 0), 2);
+            cv::line(img2_single, kp1[i].pt, kp2_single[i], cv::Scalar(0, 250, 0));
         }
     }
 
@@ -136,51 +151,64 @@ int main(int argc, char **argv) {
     cv::cvtColor(img2, img2_multi, CV_GRAY2BGR);
     for (int i = 0; i < kp2_multi.size(); i++) {
         if (success_multi[i]) {
-            cv::circle(img2_multi, kp2_multi[i].pt, 2, cv::Scalar(0, 250, 0), 2);
-            cv::line(img2_multi, kp1[i].pt, kp2_multi[i].pt, cv::Scalar(0, 250, 0));
+            cv::circle(img2_multi, kp2_multi[i], 2, cv::Scalar(0, 250, 0), 2);
+            cv::line(img2_multi, kp1[i].pt, kp2_multi[i], cv::Scalar(0, 250, 0));
         }
     }
 
-    Mat img2_CV;
-    cv::cvtColor(img2, img2_CV, CV_GRAY2BGR);
-    for (int i = 0; i < pt2.size(); i++) {
-        if (status[i]) {
-            cv::circle(img2_CV, pt2[i], 2, cv::Scalar(0, 250, 0), 2);
-            cv::line(img2_CV, pt1[i], pt2[i], cv::Scalar(0, 250, 0));
+    Mat cv_img2_sigle;
+    cv::cvtColor(img2, cv_img2_sigle, CV_GRAY2BGR);
+    for (int i = 0; i < cv_pt2_single.size(); i++) {
+        if (status_single[i]) {
+            cv::circle(cv_img2_sigle, cv_pt2_single[i], 2, cv::Scalar(0, 250, 0), 2);
+            cv::line(cv_img2_sigle, cv_pt1[i], cv_pt2_single[i], cv::Scalar(0, 250, 0));
+        }
+    }
+
+    Mat cv_img2_multi;
+    cv::cvtColor(img2, cv_img2_multi, CV_GRAY2BGR);
+    for (int i = 0; i < cv_pt2_multi.size(); i++) {
+        if (status_multi[i]) {
+            cv::circle(cv_img2_multi, cv_pt2_multi[i], 2, cv::Scalar(0, 250, 0), 2);
+            cv::line(cv_img2_multi, cv_pt1[i], cv_pt2_multi[i], cv::Scalar(0, 250, 0));
         }
     }
 
     cv::imshow("tracked single level", img2_single);
     cv::imshow("tracked multi level", img2_multi);
-    cv::imshow("tracked by opencv", img2_CV);
-    while(cv::waitKey(0)!=32);
+    cv::imshow("tracked by opencv lk single", cv_img2_sigle);
+    cv::imshow("tracked by opencv lk multi", cv_img2_multi);
+    while (cv::waitKey(0) != 32);
 
     return 0;
 }
 
 void OpticalFlowSingleLevel(
-        const Mat &img1,
-        const Mat &img2,
-        const vector<KeyPoint> &kp1,
-        vector<KeyPoint> &kp2,
-        vector<bool> &success,
+        const cv::Mat &img1,
+        const cv::Mat &img2,
+        const vector<cv::Point2f> &kpt1,
+        vector<cv::Point2f> &kpt2,
+        vector<unsigned char> &success,
         bool inverse
 ) {
     // parameters
-    int half_patch_size = 5;
-    int iterations = 20;
-    bool have_initial = !kp2.empty();
+    int half_patch_size = 4;
+    int iterations = 10;
+    bool have_initial = !kpt2.empty();
 
-    for (size_t i = 0; i < kp1.size(); i++) {
-        auto kp = kp1[i];
+    if(!success.empty())
+        success.clear();
+
+    for (size_t i = 0; i < kpt1.size(); i++) {
+        auto kpt = kpt1[i];
         double dx = 0, dy = 0; // dx,dy need to be estimated
         if (have_initial) {
-            dx = kp2[i].pt.x - kp.pt.x;
-            dy = kp2[i].pt.y - kp.pt.y;
+            dx = kpt2[i].x - kpt.x;
+            dy = kpt2[i].y - kpt.y;
         }
 
         double cost = 0, lastCost = 0;
-        bool succ = true; // indicate if this point succeeded
+        bool succ = 1; // indicate if this point succeeded
 
         // Gauss-Newton iterations
         for (int iter = 0; iter < iterations; iter++) {
@@ -188,9 +216,9 @@ void OpticalFlowSingleLevel(
             Eigen::Vector2d b = Eigen::Vector2d::Zero();
             cost = 0;
 
-            if (kp.pt.x + dx <= half_patch_size || kp.pt.x + dx >= img1.cols - half_patch_size ||
-                kp.pt.y + dy <= half_patch_size || kp.pt.y + dy >= img1.rows - half_patch_size) {   // go outside
-                succ = false;
+            if (kpt.x + dx <= half_patch_size || kpt.x + dx >= img1.cols - half_patch_size ||
+                kpt.y + dy <= half_patch_size || kpt.y + dy >= img1.rows - half_patch_size) {   // go outside
+                succ = 0;
                 break;
             }
 
@@ -201,8 +229,8 @@ void OpticalFlowSingleLevel(
                     double error = 0;
                     Eigen::Vector2d J;  // Jacobian
 
-                    float xf = kp.pt.x + x;
-                    float yf = kp.pt.y + y;
+                    float xf = kpt.x + x;
+                    float yf = kpt.y + y;
 
                     if (inverse == false) {
                         // Forward Jacobian
@@ -229,7 +257,7 @@ void OpticalFlowSingleLevel(
             if (isnan(update[0])) {
                 // sometimes occurred when we have a black or white patch and H is irreversible
                 cout << "update is nan" << endl;
-                succ = false;
+                succ = 0;
                 break;
             }
             if (iter > 0 && cost > lastCost) {
@@ -241,28 +269,28 @@ void OpticalFlowSingleLevel(
             dx += update[0];
             dy += update[1];
             lastCost = cost;
-            succ = true;
+            succ = 1;
         }
 
         success.push_back(succ);
 
         // set kp2
         if (have_initial) {
-            kp2[i].pt = kp.pt + Point2f(dx, dy);
+            kpt2[i] = kpt + cv::Point2f(dx, dy);
         } else {
-            KeyPoint tracked = kp;
-            tracked.pt += cv::Point2f(dx, dy);
-            kp2.push_back(tracked);
+            cv::Point2f tracked = kpt;
+            tracked += cv::Point2f(dx, dy);
+            kpt2.push_back(tracked);
         }
     }
 }
 
 void OpticalFlowMultiLevel(
-        const vector<Mat> &pyr1,
-        const vector<Mat> &pyr2,
-        const vector<KeyPoint> &kp1,
-        vector<KeyPoint> &kp2,
-        vector<bool> &success,
+        const vector<cv::Mat> &pyr1,
+        const vector<cv::Mat> &pyr2,
+        const vector<cv::Point2f> &kpt1,
+        vector<cv::Point2f> &kpt2,
+        vector<unsigned char> &success,
         bool inverse) {
 
     // parameters
@@ -271,21 +299,21 @@ void OpticalFlowMultiLevel(
     double pyramid_scale = pyr1[1].cols / (double)pyr1[0].cols;
 
     // coarse-to-fine LK tracking in pyramids
-    size_t size_kp1 = kp1.size();
-    vector<KeyPoint> kp1_top;
-    kp1_top.reserve(size_kp1);
+    size_t size_kp1 = kpt1.size();
+    vector<cv::Point2f> kpt1_top;
+    kpt1_top.reserve(size_kp1);
     for (int i = 0; i<size_kp1; i++) {
-        KeyPoint kp = kp1[i];
-        kp.pt *= std::pow(pyramid_scale, pyramids-1);
-        kp1_top.push_back(kp);
+        cv::Point2f kpt = kpt1[i];
+        kpt *= std::pow(pyramid_scale, pyramids-1);
+        kpt1_top.push_back(kpt);
     }
     for (int l=pyramids-1; l>=0; l--) {
         if(l < pyramids-1) {
-            for (int i = 0; i < kp2.size(); i++) {
-                kp1_top[i].pt /= pyramid_scale;
-                kp2[i].pt     /= pyramid_scale;
+            for (int i = 0; i < kpt2.size(); i++) {
+                kpt1_top[i] /= pyramid_scale;
+                kpt2[i]     /= pyramid_scale;
             }
         }
-        OpticalFlowSingleLevel(pyr1[l], pyr2[l], kp1_top, kp2, success, inverse);
+        OpticalFlowSingleLevel(pyr1[l], pyr2[l], kpt1_top, kpt2, success, inverse);
     }
 }
